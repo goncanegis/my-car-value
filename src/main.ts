40a77389ea7f4db762c975cdafd370d4771df12d
Gonca Negis <gonca.negis@gmail.com>
2023-08-13 10:18:02 +0300

    added app access to cookie session - will refactor keys

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      // Only for development purposes. Will refactor later.
+      keys: ['henloo'],
+    }),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // This will ignore any properties that are not in the DTO. This will prevent the user from sending extra properties that are not in the DTO.
